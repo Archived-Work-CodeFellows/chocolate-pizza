0fbc27fb368e36fba503d53f01ecb4a2341901f2
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-02-08 19:07:44 -0800

    Added some bars

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var milk = document.getElementById('milk');
+
+// var checkList = document.getElementById('checklist');
+// console.log(checkList);
+// function checkListHandler(event){
+//   //event.preventDefault();
+//   console.log(event);
+
+//   var checkMilk = event.target.milk.value;
+//   console.log(checkMilk);
+// }
+
+// checkListHandler('onclick', checkList);
